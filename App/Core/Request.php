1cc464df226f114fbc0d5828b38d067bf30a56e9
shirinkalam <mahdishiri869@gmail.com>
2022-07-04 10:08:10 +0430

    Request-Finished

--- App/Core/Request.php
+++ App/Core/Request.php
@@ -4,8 +4,52 @@ namespace App\Core;
 
 class Request 
 {
+    private $params;
+    private $method;
+    private $agent;
+    private $ip;
+    private $uri;
+
     public function __construct()
     {
-        echo "Request Class";
+        $this->params = $_REQUEST;
+        $this->agent  = $_SERVER['HTTP_USER_AGENT'];
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->ip     = $_SERVER['REMOTE_ADDR'];
+        $this->uri     = strtok($_SERVER['REQUEST_URI'] , '?');
+        
+    }
+
+    public function params(){
+        return $this->params;
+    }
+
+    public function method(){
+        return $this->method;
+    }
+
+    public function agent(){
+        return $this->agent;
+    }
+
+    public function ip(){
+        return $this->ip;
+    }
+
+    public function uri(){
+        return $this->uri;
+    }
+
+    public function input($key){
+        return $this->params[$key] ?? null;
+    }
+
+    public function isset($key){
+        return isset( $this->params[$key]);
+    }
+
+    public function redirect($route){
+        header("Location: " . site_url($route));
+        die();
     }
 }
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 #Front Controller
-use App\Utilities\Url;
 use App\Core\StupidRouter;
+
 include 'bootstrap/init.php';
 
-$router= new StupidRouter();
-$router->run();
+// $router= new StupidRouter();
+// $router->run();
+
+// $request = new \App\Core\Request();
+// $request->redirect('colors/red');
+
