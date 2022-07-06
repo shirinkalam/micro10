93653adabe50cdc6eafe66c44cda17371ef3bf09
shirinkalam <mahdishiri869@gmail.com>
2022-07-06 18:45:25 +0430

    create-BaseModels

---
+++ App/Models/Contracts/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models\Contracts;
+
+abstract class BaseModel implements CRUDInterface{
+    
+    protected $connection;
+    protected $table;
+    protected $primaryKey = 'id';
+    protected $pageSize= '10';
+    protected $attributes= [];
+
+    public function __construct(){
+        #if MYSQL = set mysql connection
+    }
+
+    protected function getAttributes($key){
+        if(!$key or array_key_exists($key , $this->attributes)){
+            return null;
+        }
+        return $this->attributes[$key];
+    }
+
+}
--- App/Models/Contracts/CRUDInterface.php
+++ App/Models/Contracts/CRUDInterface.php
@@ -5,11 +5,14 @@ namespace App\Models\Contracts;
 interface CRUDInterface{
     #Create(INSERT)
     public function create(array $data) : int;
+
     #Read(SELECT : single OR multiple)
     public function read($id) : object;
     public function get(array $columns,array $where) : array;
+    
     #Update(UPDATE)
     public function update(array $data,array $where) : int;
+    
     #Delete(DELETE)
     public function delete(array $where) : int;
 }
