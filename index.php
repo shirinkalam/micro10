db45b726f63adf6c1ecae7cea6555684a35a1162
shirinkalam <mahdishiri869@gmail.com>
2022-07-09 19:51:10 +0430

    delete-contact-finished

--- index.php
+++ index.php
@@ -7,16 +7,5 @@ use App\Models\Product;
 
 include 'bootstrap/init.php';
 
-
-// $productModel = new Product();
-
-// for($i =1 ;$i<=7;$i++){
-//     $productModel->create([
-//         'title' =>"Product-$i",
-//         'price' =>rand(1,100)*1000
-//     ]);
-// }
-// $result = $userModel->delete(['name'=>'mahdi'],['id'=>1]);
-
 $router = new \App\Core\Routing\Router();
 $router->run();
